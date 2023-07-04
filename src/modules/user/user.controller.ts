a9202134e16ccd78f072e08278f9a48d78021cf4
Ivor Baric <ivor@farshore.com>
2023-07-04 15:54:18 +0200

    feature push

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Req } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
+import { CommonApiResponse } from '../../helpers/decorators/api-response-swagger.decorator';
+import { UserProfileDto } from './dto/user-profile.dto';
 
 @ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @ApiBearerAuth()
+  @Get('me')
+  @CommonApiResponse(UserProfileDto)
+  findMe(@Req() request) {
+    console.log(request.user);
+    return UserProfileDto.fromEntityUser(request.user);
+  }
 }
