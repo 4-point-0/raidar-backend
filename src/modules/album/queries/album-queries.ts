8b6ed1b5639b3caf8dcb679439a3c22aaeaa2344
Ivor Baric <ivor@farshore.com>
2023-08-03 17:22:21 +0200

    adding the /me endpoint for album

--- src/modules/album/queries/album-queries.ts
+++ src/modules/album/queries/album-queries.ts
@@ -17,6 +17,30 @@ export const findAllAlbumsQuery = (take: number, skip: number) => {
   };
 };
 
+export const findAllArtistAlbumsQuery = (
+  take: number,
+  skip: number,
+  id: string,
+) => {
+  return {
+    relations: {
+      cover: true,
+      songs: {
+        user: true,
+        music: true,
+        art: true,
+        listings: {
+          buyer: true,
+          seller: true,
+        },
+      },
+    },
+    take: take,
+    skip: skip,
+    where: { created_by_id: id },
+  };
+};
+
 export const findOneAlbumQuery = (id: string) => {
   return {
     where: {
