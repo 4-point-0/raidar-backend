bcc515be097e92baaa3cf909a1d59d7ef8e8ce24
rimatik <slaven@rimatik.hr>
2023-09-29 12:55:10 +0200

    changed templates

--- src/common/email-templates/song-bought-notif-template.ts
+++ src/common/email-templates/song-bought-notif-template.ts
@@ -1,31 +1,44 @@
-export const songBoughtTemplate = (songName: string) => `<!DOCTYPE html>
+export const songBoughtTemplate = (songName: string) => `
+<!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Song Sold Notification</title>
-    <style>
-      body {
-        font-family: Arial, sans-serif;
-        background-color: #f4f4f4;
-        margin: 0;
-        padding: 0;
-      }
-      .container {
-        background-color: #fff;
-        border-radius: 8px;
-        margin: 20px auto;
-        max-width: 600px;
-        padding: 20px;
-        text-align: center;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <h1>Congratulations, Your Song Was Bought!</h1>
-      <p>Your song "${songName}" has been purchased!</p>
-      <p>Sincerely,</p>
-      <p>The Raidar Team</p>
-    </div>
-  </body>
-</html>`;
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+  <title>Song Sold Notification</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f4f4f4;
+      margin: 0;
+      padding: 0;
+    }
+
+    .container {
+      background-color: #fff;
+      border-radius: 8px;
+      margin: 20px auto;
+      max-width: 600px;
+      padding: 20px;
+      text-align: center;
+    }
+
+    #built-on-near {
+      width: 250px;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="container">
+    <img src="https://app.raidar.us/images/berklee-logo.svg" alt="Berklee Logo" />
+    <h1>Congratulations, Your Song Was Bought!</h1>
+    <p>Your song "${songName}" has been purchased!</p>
+    <p>Sincerely,</p>
+    <p>The Raidar Team</p>
+    <img id="built-on-near" src="https://app.raidar.us/images/built-on-near-black.svg" alt="Built on NEAR Logo" />
+  </div>
+</body>
+
+</html>
+`;
--- src/common/email-templates/song-dowload-template.ts
+++ src/common/email-templates/song-dowload-template.ts
@@ -1,44 +1,64 @@
-export const songDownloadTemplate = (downloadUrl: string) => `<!DOCTYPE html>
+export const songDownloadTemplate = (songName: string, downloadUrl: string) => `
+<!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Your Song Download Link</title>
-    <style>
-      body {
-        font-family: Arial, sans-serif;
-        background-color: #f4f4f4;
-        margin: 0;
-        padding: 0;
-      }
-      .container {
-        background-color: #fff;
-        border-radius: 8px;
-        margin: 20px auto;
-        max-width: 600px;
-        padding: 20px;
-        text-align: center;
-      }
-      .button {
-        background-color: #007bff;
-        border: none;
-        border-radius: 4px;
-        color: white;
-        padding: 15px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <h1>Your Song Purchase is Complete!</h1>
-      <p>Thank you for purchasing the song.</p>
-      <p>Click the link below to download:</p>
-      <a href="${downloadUrl}" class="button">Download Song</a>
-      <p>Sincerely,</p>
-      <p>The Raidar Team</p>
-    </div>
-  </body>
-</html>`;
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+  <title>Your Song Download Link</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f4f4f4;
+      margin: 0;
+      padding: 0;
+    }
+
+    .container {
+      background-color: #fff;
+      border-radius: 8px;
+      margin: 20px auto;
+      max-width: 600px;
+      padding: 20px;
+      text-align: center;
+    }
+
+    .button {
+      background-color: #ff6b6b;
+      border: none;
+      border-radius: 4px;
+      color: #ffffff;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      font-weight: 500;
+      letter-spacing: 2px;
+    }
+
+    .button:hover {
+      filter: brightness(110%);
+    }
+
+    #built-on-near {
+      width: 250px;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="container">
+    <img src="https://app.raidar.us/images/berklee-logo.svg" alt="Berklee Logo"/>
+    <h1>Your Song Purchase is Complete!</h1>
+    <p>Thank you for purchasing the song ${songName}.</p>
+    <p>Click the link below to download:</p>
+    <a href="${downloadUrl}" class="button">Download Song</a>
+    <p>Sincerely,</p>
+    <p>The Raidar Team</p>
+    <img id="built-on-near" src="https://app.raidar.us/images/built-on-near-black.svg" alt="Built on NEAR Logo" />
+  </div>
+</body>
+
+</html>
+`;
