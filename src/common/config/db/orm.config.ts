05a53d3ad0fe56a76fa711797c3e32c2f802d3d9
Ivor Baric <ivor@farshore.com>
2024-09-04 16:46:26 +0200

    updating migrations bug

--- src/common/config/db/orm.config.ts
+++ src/common/config/db/orm.config.ts
@@ -1,6 +1,6 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { data_source_config } from './db';
 
-export const AppDataSource = new DataSource({
-  ...(data_source_config as DataSourceOptions),
-});
+const AppDataSource = new DataSource(data_source_config as DataSourceOptions);
+
+export default AppDataSource;
